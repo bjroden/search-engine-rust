d6adc45eabf90e26cfd6f63d50622d4a00e70919
bjroden <bjroden@uark.edu>
2022-08-06 23:25:28 -0500

    Tokenize directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::{fs::{File, self}, io::Read, env};
+
 use crate::parser::parse;
 
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
-    let tokens = parse("Create ridiculously fast lexers <body>1 1a2 13.5 <b>E</b>lephants.");
-    for token in tokens {
-        println!("{}", token);
+    let args: Vec<String> = env::args().collect();
+    let indir = args.get(1).expect("Indir not given");
+    for file_path in fs::read_dir(indir).expect("Could not read indir") {
+        println!("----- File Path: {:?} -----", file_path.as_ref().unwrap().file_name());
+        let mut file = File::open(file_path.unwrap().path()).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let tokens = parse(&contents.as_str());
+        for token in tokens {
+            println!("{}", token);
+        }
     }
 }
