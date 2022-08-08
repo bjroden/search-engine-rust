bc115c905c57a886e17f901f067fdf84a0b42e88
bjroden <bjroden@uark.edu>
2022-08-08 15:56:41 -0500

    Add argument parsing to query

--- src/query.rs
+++ src/query.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    directory: String,
+
+    #[clap(short, long, value_parser)]
+    query: String,
+
+    #[clap(short, long, value_parser, default_value_t = 10)]
+    num_results: usize
+}
+
 fn main() {
-    println!("Hello world");
+    let args = Args::parse();
+    println!("{}", args.query);
 }
