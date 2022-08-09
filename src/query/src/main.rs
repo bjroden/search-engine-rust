b69139f832e4dec9a59da8f854ec9b78a7c85885
bjroden <bjroden@uark.edu>
2022-08-09 18:19:13 -0500

    Put file read logic into utils

--- src/query/src/main.rs
+++ src/query/src/main.rs
@@ -1,15 +1,6 @@
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
-use std::fs::File;
-use std::io::{Error, BufReader, Seek, SeekFrom, BufRead};
-use std::vec;
 
 use clap::Parser;
-use util::parser::parse;
-use util::data_models::{DictRecord, PostRecord};
-use util::hashtable::{hash_function, rehash, HashTable};
-use util::constants::*;
-
+use util::read_query_files::make_query;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -23,134 +14,9 @@ struct Args {
     num_results: usize
 }
 
-fn get_query_tokens(query: &str) -> Vec<String> {
-    return parse(query);
-}
-
-fn get_dict_records(filedir: &str, tokens: &Vec<String>) -> Result<Vec<DictRecord>, Error> {
-    let mut records = vec![];
-    let file = File::open(format!("{filedir}/dict"))?;
-    let mut reader = BufReader::new(file);
-    for token in tokens {
-        records.push(get_one_dict_record(&mut reader, token)?);
-    }
-    Ok(records)
-}
-
-fn get_one_dict_record(reader: &mut BufReader<File>, token: &str) -> Result<DictRecord, Error> {
-    let mut hash = hash_function(token, &GLOB_HT_SIZE).unwrap();
-    let mut record = read_one_dict_line_from_hash(reader, hash)?;
-    while record.term != "!NULL" && record.term != token { 
-        hash = rehash(&hash, &GLOB_HT_SIZE);
-        record = read_one_dict_line_from_hash(reader, hash)?;
-    }
-    Ok(record)
-}
-
-fn read_one_dict_line_from_hash(reader: &mut BufReader<File>, hash: usize) -> Result<DictRecord, Error> {
-    reader.seek(SeekFrom::Start((hash * DICT_RECORD_SIZE).try_into().unwrap()))?;
-    let mut record_str = String::new();
-    reader.read_line(&mut record_str)?;
-    let split_record: Vec<&str> = record_str.split_whitespace().collect();
-    let term = split_record[0];
-    let num_docs = split_record[1].parse().unwrap();
-    let start = split_record[2].parse().unwrap();
-    Ok(DictRecord { term: term.to_string(), num_docs: num_docs, post_line_start: start })
-}
-
-fn make_query_ht(post_records: &Vec<PostRecord>, expected_docs: usize) -> HashTable<usize> {
-    let mut query_ht = HashTable::new(expected_docs * 3);
-    for record in post_records {
-        query_ht.insert_combine(&record.doc_id.to_string(), record.weight);
-    }
-    query_ht
-}
-
-fn get_all_post_records(filedir: &str, dict_records: &Vec<DictRecord>) -> Result<Vec<PostRecord>, Error> {
-    let file = File::open(format!("{filedir}/post"))?;
-    let mut reader = BufReader::new(file);
-    let mut post_records = vec![];
-    for dict_record in dict_records {
-        post_records.append(&mut get_term_post_records(&mut reader, &dict_record)?);
-    }
-    Ok(post_records)
-}
-
-fn get_term_post_records(reader: &mut BufReader<File>, dict_record: &DictRecord) -> Result<Vec<PostRecord>, Error> {
-    let mut post_records = vec![];
-    reader.seek(SeekFrom::Start((dict_record.post_line_start * POST_RECORD_SIZE).try_into().unwrap()))?;
-    for _ in 0..dict_record.num_docs {
-        let mut record_str = String::new();
-        reader.read_line(&mut record_str)?;
-        let split_record: Vec<&str> = record_str.split_whitespace().collect();
-        let doc_id = split_record[0].parse().unwrap();
-        let weight: usize = split_record[1].parse().unwrap();
-        post_records.push(PostRecord { doc_id: doc_id, weight: weight })
-    }
-    Ok(post_records)
-}
-
-fn get_sorted_results(query_ht: HashTable<usize>, num_results: usize) -> Vec<PostRecord> {
-    let mut heap: BinaryHeap<Reverse<PostRecord>> = BinaryHeap::new();
-    for bucket in query_ht.get_buckets() {
-        if let Some(entry) = bucket {
-            match heap.peek() {
-                Some(Reverse(heap_head)) => {
-                    if heap.len() + 1 > num_results {
-                        if heap_head.weight < entry.value { 
-                            heap.pop();
-                            heap.push(Reverse(PostRecord { doc_id: entry.key.parse().unwrap(), weight: entry.value }));
-                        }
-                    }
-                    else {
-                        heap.push(Reverse(PostRecord { doc_id: entry.key.parse().unwrap(), weight: entry.value }));
-                    }
-                }
-                None => heap.push(Reverse(PostRecord { doc_id: entry.key.parse().unwrap(), weight: entry.value }))
-            }
-        }
-    }
-    let rev_sorted = heap.into_sorted_vec();
-    let mut sorted = vec![];
-    for rev_elem in rev_sorted {
-        if let Reverse(elem) = rev_elem {
-            sorted.push(elem.clone());
-        }
-    }
-    sorted
-}
-
-struct NamedResult {
-    name: String,
-    weight: usize
-}
-
-fn get_named_results(filedir: &str, results: Vec<PostRecord>) -> Result<Vec<NamedResult>, Error> {
-    let mut named_results = vec![];
-    let file = File::open(format!("{filedir}/map"))?;
-    let mut reader = BufReader::new(file);
-    for result in results {
-        named_results.push(NamedResult { name: get_doc_name(&mut reader, result.doc_id)?, weight: result.weight })
-    }
-    Ok(named_results)
-}
-
-fn get_doc_name(reader: &mut BufReader<File>, doc_id: usize) -> Result<String, Error> {
-    let mut name = String::new();
-    reader.seek(SeekFrom::Start((doc_id * MAP_RECORD_SIZE).try_into().unwrap()))?;
-    reader.read_line(&mut name)?;
-    Ok(name.trim().to_string())
-}
-
 fn main() {
     let args = Args::parse();
-    let tokens = get_query_tokens(&args.query);
-    let dict_records = get_dict_records(&args.directory, &tokens).expect("Error reading dict file");
-    let expected_docs = dict_records.iter().fold(0, |sum, record| sum + record.num_docs);
-    let post_records = get_all_post_records(&args.directory, &dict_records).expect("Error reading post file");
-    let query_ht = make_query_ht(&post_records, expected_docs);
-    let sorted_results = get_sorted_results(query_ht, args.num_results);
-    for (num, result) in get_named_results(&args.directory, sorted_results).expect("Error reading map file").iter().enumerate() {
+    for (num, result) in make_query(&args.query, &args.directory, args.num_results).expect("Error reading files").iter().enumerate() {
         println!("{}: {} (weight: {})", num + 1, result.name, result.weight);
     }
 }
--- src/util/src/lib.rs
+++ src/util/src/lib.rs
@@ -2,3 +2,4 @@ pub mod constants;
 pub mod data_models;
 pub mod hashtable;
 pub mod parser;
+pub mod read_query_files;
